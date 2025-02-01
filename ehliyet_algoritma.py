e4d0521f1765c7cd7ec05f71418a6285a82b4e9d
Mert Karaosmanoğlu <mertkaraosmanoglu745@gmail.com>
2025-02-02 02:23:24 +0300

    yorum satırları eklendi

--- ehliyet_algoritma.py
+++ ehliyet_algoritma.py
@@ -1,13 +1,22 @@
 def ehliyet_kontrol(yas):
-    if yas >= 18:
-        return "Ehliyet alabilir."
+    """
+    Bu fonksiyon, kullanicinin yasina gore ehliyet alip alamayacagini belirler.
+
+    Args:
+        yas (int): Kullanicinin yasi.
+
+    Returns:
+        str: Kullanicinin ehliyet durumu hakkinda bir mesaj.
+    """
+    if yas >= 18:   # Yas 18 veya daha buyukse
+        return "Ehliyet alabilir."  # Ehliyet alabilir mesajini dondur
     else:
-        return "Ehliyet alamaz."
+        return "Ehliyet alamaz."    # Ehliyet alamaz mesajini dondur
 
 if __name__ == "__main__":
     try:
-        kullanici_yasi = int(input("Lütfen yasinizi giriniz: "))
-        sonuc = ehliyet_kontrol(kullanici_yasi)
+        kullanici_yasi = int(input("Lutfen yasinizi giriniz: "))    # Kullanicidan yas bilgisini al
+        sonuc = ehliyet_kontrol(kullanici_yasi)     # Kullanicinin yasini fonksiyona gonder ve sonucu al
         print(sonuc)
-    except ValueError:
-        print("Lütfen geçerli bir sayi giriniz.")
+    except ValueError:      # Kullanici gecerli bir sayi girmemisse
+        print("Lutfen gecerli bir sayi giriniz.")
